ace6a2eede4d0944bec87e6d5981f139fb8a26d6
linstyle <lin_style@foxmail.com>
2012-08-10 00:18:03 +0800

    首页

--- jzns/config/application.rb
+++ jzns/config/application.rb
@@ -55,5 +55,11 @@ module Jzns
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+		config.time_zone = 'Beijing'
+		config.active_record.default_timezone = :local
+		config.active_record.time_zone_aware_attributes = false # this is important!
+		
+		# config.autoload_paths += %W(#{config.root}/lib)    
   end
 end
---
+++ sqlback/back.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Community v9.61 
+MySQL - 5.5.22-log : Database - jzns
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`jzns` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+
+USE `jzns`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `name` char(24) COLLATE utf8_unicode_ci NOT NULL COMMENT '用户名',
+  `password_digest` char(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '密码',
+  `nick_name` char(10) COLLATE utf8_unicode_ci NOT NULL COMMENT '用户昵称',
+  `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '注册(插入)时间',
+  PRIMARY KEY (`id`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
