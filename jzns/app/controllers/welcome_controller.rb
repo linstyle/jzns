7b3352b3baf2635fd2b5bc704978fd6da3f05048
lin_style <lin_style@foxmail.com>
2012-08-10 14:39:11 +0800

    首页

--- jzns/app/controllers/welcome_controller.rb
+++ jzns/app/controllers/welcome_controller.rb
@@ -1,5 +1,55 @@
+#encoding:utf-8
 class WelcomeController < ApplicationController
-	def index
+  #用户首页
+  def index
 		@user = User.new
+
+	  if '1'==cookies[:remeber_me_check]
+			user_fromcookies = User.find_by_id( cookies[:remember_me_id] )
+			
+			if user_fromcookies && Digest::SHA1.hexdigest( user_fromcookies.name )[3,18] == cookies[:remember_me_code]
+				session[:user_id] = user_fromcookies.id 
+				login_process(user_fromcookies)
+				redirect_to(:action => "user_index")
+			end
+		end
+		
   end
+  
+  #登陆
+  def login	
+		@user_params = User.new(params[:user])
+		user = User.find_by_name(@user_params.name)
+		if user && user.status==0 && user.authenticate(@user_params.password) 
+			login_by_login_create(user, params[:remember_me_check])
+		else
+			flash[:notice] =  "无效的用户名或密码"
+			redirect_to(:action => "index")
+		end		  
+	end  
+  
+  #处理登陆需要做的事情
+	def login_process(user)
+		#更新登陆时间
+		if !user.update_attribute("lastlogin_time", Time.now)
+			logger.error("update lastlogin_time failed. userid:#{user.id}")
+		end			
+	  #显示消息类型默认为0
+  end 
+	
+	def login_by_login_create(user, check_me)
+		login_process(user)
+		#保存cookies
+		if check_me
+			cookies[:remeber_me_check] = {:value=>1.to_s, :expires => 7.days.from_now}
+			userid_str = (user.id).to_s
+			cookies[:remember_me_id] = { :value => userid_str, :expires => 7.days.from_now }    
+			usercode_encry = Digest::SHA1.hexdigest( user.name )[3,18]    
+			cookies[:remember_me_code] = { :value => usercode_encry, :expires => 7.days.from_now }
+		end  
+			  
+		session[:user_id] = user.id
+		redirect_to(:action => "user_index")
+  end
+  	  
 end
--- sqlback/back.sql
+++ sqlback/back.sql
@@ -1,6 +1,6 @@
 /*
-SQLyog Community v9.61 
-MySQL - 5.5.22-log : Database - jzns
+SQLyog Community Edition- MySQL GUI v7.15 
+MySQL - 5.5.23-log : Database - jzns
 *********************************************************************
 */
 
@@ -8,10 +8,9 @@ MySQL - 5.5.22-log : Database - jzns
 
 /*!40101 SET SQL_MODE=''*/;
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`jzns` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 
 USE `jzns`;
@@ -25,6 +24,7 @@ CREATE TABLE `users` (
   `name` char(24) COLLATE utf8_unicode_ci NOT NULL COMMENT '用户名',
   `password_digest` char(60) COLLATE utf8_unicode_ci NOT NULL COMMENT '密码',
   `nick_name` char(10) COLLATE utf8_unicode_ci NOT NULL COMMENT '用户昵称',
+  `lastlogin_time` datetime DEFAULT '2012-08-10 00:00:00' COMMENT '最后登录时间',
   `reg_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '注册(插入)时间',
   PRIMARY KEY (`id`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -33,5 +33,3 @@ CREATE TABLE `users` (
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
