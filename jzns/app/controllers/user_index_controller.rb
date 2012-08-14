67541cc84bb5a31288e66d43054263c24c6b5b6d
lin_style <lin_style@foxmail.com>
2012-08-14 14:12:25 +0800

    头部

--- jzns/app/controllers/user_index_controller.rb
+++ jzns/app/controllers/user_index_controller.rb
@@ -1,5 +1,5 @@
 class UserIndexController < ApplicationController
-  before_filter :find_user, :only => [ :user_index]
+  before_filter :find_user, :only => [:user_index,:common_event,:person_event,:collect_event,:setting,:about]
   def find_user
   	@user = User.find_by_id(session[:user_id])
   end  
@@ -8,5 +8,38 @@ class UserIndexController < ApplicationController
   def user_index
   	userid=session[:user_id]
 	  
+	  common_event()
   end
+  
+  #公共事件
+  def common_event
+  	@select_link=0
+  	
+  end
+  
+  #个人事件
+  def person_event
+  	@select_link=1
+  	
+  end
+  
+  #我的关注(收藏)
+  def collect_event
+		@select_link=2
+		
+  end
+  
+  #设置
+  def setting
+		@select_link=3
+		  
+  end
+  
+  #关于
+  def about
+		@select_link=4
+		  
+  end
+  
+  
 end
