0b717d13e7cea56f18ece197ca5a9b0277510098
= <=>
2012-09-23 17:02:37 +0800

    add shell

---
+++ fitting/auto_release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cp -r /home/lindp/Programmer/GitHub/jzns/jzns /home/lindp/Programmer/Release/
+cd /home/lindp/Programmer/Release/jzns/
+
+rm tmp/* -rf
+rm log/* -rf
+rm public/system -rf
+
+month=`date +%m`
+day=`date +%d`
+hour=`date +%k`
+min=`date +%M`
+
+release_file_name=jzns-$month-$day-$hour-$min.tar
+tar -cvf /home/lindp/Programmer/Release/$release_file_name /home/lindp/Programmer/Release/jzns/
